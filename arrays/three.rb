8cf1ba397d93aa071dabfc1eeb201c2c06804743
Justin Kim <justinkim2015@gmail.com>
2023-02-16 14:31:55 +0900

    Write pseudocode

--- arrays/three.rb
+++ arrays/three.rb
@@ -1,3 +1,13 @@
 def two_sum(nums, target)
-    
+  nums.each_with_index do |num, index|
+  end
 end
+
+two_sum([3,2,3], 6)
+# First check first number vs second number
+  # If the sum of the two is target end the program
+# Check first num vs third num...
+
+# If first number + any of the other numbers doesn't equal the target save the first number into an array
+# If second number is present in the array skip it
+# etc...
