623161e402af8b44d9dd563f871aae805bfb846b
Justin Kim <justinkim2015@gmail.com>
2023-02-17 11:20:47 +0900

    Add solution that counts letters

--- arrays/group_anagrams.rb
+++ arrays/group_anagrams.rb
@@ -1,13 +1,41 @@
-def group_anagrams(strs)
-end
+# Sort
+# def group_anagrams(strs)
+#   hash = {}
+#   strs.each do |str|
+#     if hash[str.split('').sort].nil?
+#       hash[str.split('').sort] = [str]
+#     else 
+#       hash[str.split('').sort] << str
+#     end
+#   end
+#   p hash.values
+# end
+
+# def group_anagrams(strs)
+#   hash = Hash.new {|h, k| h[k] = []}
+#   strs.each do |str|
+#     hash[str.split('').sort] << str
+#   end
+#   p hash.values
+# end
 
 # Plan
-# First make a hash
-# take each string and split and sort it
-# Save the string as the key and the split/sorted array as the value.
-# Now I have a hash of string:array pairs
-# Take my first hash key and put it in an array.
-# check to see if my hash value matches any other hash value in the hash.
-# If so push the second has key to my array and go through the entire hash this way.
-# Push entire array to final array and repeat
+# First make a hash map
+# take each string and split and sort it 
+# Save the  split/sorted array as the key and make the value an array x
+# Add the values if the key is the same
 
+# Counting
+def group_anagrams(strs)
+  anagrams = Hash.new { |h, k| h[k] = [] }
+  strs.each do |str|
+    hash = Hash.new(0)
+    str.each_char { |c| hash[c] += 1 }
+    anagrams[hash] << str
+  end
+  anagrams.values
+end
+
+strs = ["eat","tea","tan","ate","nat","bat"]
+
+group_anagrams(strs)
