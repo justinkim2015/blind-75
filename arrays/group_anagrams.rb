797e8d65c2796cd4f049b65cedb76af54ef3ab27
Justin Kim <justinkim2015@gmail.com>
2023-02-17 10:33:27 +0900

    Write initial plan to solve

--- arrays/group_anagrams.rb
+++ arrays/group_anagrams.rb
@@ -1,3 +1,13 @@
 def group_anagrams(strs)
-    
 end
+
+# Plan
+# First make a hash
+# take each string and split and sort it
+# Save the string as the key and the split/sorted array as the value.
+# Now I have a hash of string:array pairs
+# Take my first hash key and put it in an array.
+# check to see if my hash value matches any other hash value in the hash.
+# If so push the second has key to my array and go through the entire hash this way.
+# Push entire array to final array and repeat
+
