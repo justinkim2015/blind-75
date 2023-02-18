1c69f905f78c027cae9ead1d7da3b9a50e61c7c6
Justin Kim <justinkim2015@gmail.com>
2023-02-18 19:00:38 +0900

    Solve problem using sort and hash table

--- arrays/top_k.rb
+++ arrays/top_k.rb
@@ -1,16 +1,26 @@
 def top_k_frequent(nums, k)
   count = Hash.new(0)
-
+  final = []
   nums.each do |num|
     count[num] += 1
   end
+
+  sorted = count.sort_by {|k, v| v}.reverse
+
+  k.times do |i|
+    final << sorted[i][0]
+  end
+
+  final
 end
 
-nums = [1,1,1,2,2,3]
+# nums = [1,1,1,2,2,3]
+nums = [4,1,-1,2,-1,2,3]
+
 k = 2
 
 top_k_frequent(nums, k)
 
 # strategy
 # make a hashmap counting the number of each element
-# return the two highest values
+# return the K highest values
