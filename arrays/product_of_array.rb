27f504b88195278a19b7872486f5a209ce9258a2
Justin Kim <justinkim2015@gmail.com>
2023-02-19 22:15:11 +0900

    Solve via brute force

--- arrays/product_of_array.rb
+++ arrays/product_of_array.rb
@@ -1,4 +1,22 @@
+# Brute force method
 def product_except_self(nums)
+  array = []
+  i = 0
+
+  nums.length.times do 
+    final = 1
+
+    nums.each_with_index do |num, index|
+      next if i == index 
+
+      final *= num
+    end 
+
+    array << final 
+    i += 1
+  end
+
+  array
 end
 
 nums = [1,2,3,4]
