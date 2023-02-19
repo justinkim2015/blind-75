7a85fbbf4ccbf374be8b63adf2d40f596e766a05
Justin Kim <justinkim2015@gmail.com>
2023-02-19 22:30:02 +0900

    Use prefix and postfix

--- arrays/product_of_array.rb
+++ arrays/product_of_array.rb
@@ -1,22 +1,26 @@
 # Brute force method
+# def product_except_self(nums)
+#   array = []
+#   i = 0
+
+#   nums.length.times do 
+#     final = 1
+
+#     nums.each_with_index do |num, index|
+#       next if i == index 
+
+#       final *= num
+#     end 
+
+#     array << final 
+#     i += 1
+#   end
+
+#   array
+# end
+
+# Prefix and Postfix
 def product_except_self(nums)
-  array = []
-  i = 0
-
-  nums.length.times do 
-    final = 1
-
-    nums.each_with_index do |num, index|
-      next if i == index 
-
-      final *= num
-    end 
-
-    array << final 
-    i += 1
-  end
-
-  array
 end
 
 nums = [1,2,3,4]
