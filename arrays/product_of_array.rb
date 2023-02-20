25078dbec3e88b962f8b8f0b7ac2b0ad00627ae8
Justin Kim <justinkim2015@gmail.com>
2023-02-20 17:59:20 +0900

    Write code using post and prefix

--- arrays/product_of_array.rb
+++ arrays/product_of_array.rb
@@ -21,6 +21,29 @@
 
 # Prefix and Postfix
 def product_except_self(nums)
+  pre = []
+  total_pre = 1
+  nums.each do |num|
+    pre << total_pre
+    total_pre *= num
+  end
+
+  post = []
+  total_post = 1
+  (nums.length - 1).downto(0) do |index|
+    post << total_post
+    total_post *= nums[index]
+  end 
+
+  final = []
+
+  i = 0
+  nums.length.times do
+    final << pre[i] * post[nums.length - 1 - i]
+    i += 1
+  end
+
+  final
 end
 
 nums = [1,2,3,4]
