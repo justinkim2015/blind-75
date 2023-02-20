c707cd5a2600db5eddc860c80bb9572317491f98
Justin Kim <justinkim2015@gmail.com>
2023-02-20 18:42:04 +0900

    brute force basic problem

--- arrays/longest_consecutive.rb
+++ arrays/longest_consecutive.rb
@@ -3,31 +3,44 @@ def longest_consecutive(nums)
 
   sorted_list = nums.sort
   current = sorted_list[0]
-
-  consecutive = 1
+  final = [] 
+  arr = [sorted_list[0]]
 
   sorted_list.each_with_index do |num, index|
-    next if index === 0 || current === num
+    next if index === 0
 
     if current + 1 == num
-      p current
-      p num
-      consecutive += 1
-      current = num
-    else 
-      return consecutive
+      arr << num
+    else
+      final << arr
+      arr = []
     end
+
+    current = num
   end
 
-  consecutive
+  final << arr 
+
+  longest = 0
+  final.each do |consecutive|
+    if consecutive.length > longest
+      longest = consecutive.length
+    end 
+  end 
+
+  p final
+  p longest
 end
 
 # nums = [100,4,200,1,3,2]
 # nums = [0,3,7,2,5,8,4,6,0,1]
-
-nums = [9,1,4,7,3,-1,0,5,8,-1,6]
+nums = [9,1,4,7,3,-1,-1, 0,5,8,-1,6]
+# p nums.sort
 
 longest_consecutive(nums)
 
-p nums.sort
-# expected output 4
+# Strategy
+# Break array in many smaller arrays of sequential numbers
+# Find array with longest length then return that arrays length
+
+# GLITCH IS THE LOWEST VALUE REPEATING DOESNT WORK
