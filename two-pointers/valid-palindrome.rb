b6a91427f3fac9b3af12ae2a216d482900e66f51
Justin Kim <justinkim2015@gmail.com>
2023-03-23 13:16:25 +0900

    Solve with regex

--- two-pointers/valid-palindrome.rb
+++ two-pointers/valid-palindrome.rb
@@ -1,3 +1,11 @@
 def is_palindrome(s)
-    true
+    str = s.delete('^A-Za-z0-9').downcase
+    return true if str.reverse == str
+
+    false
 end
+
+string = 'racecar'
+s = "A man, a plan, a canal: Panama"
+
+p is_palindrome(s)
